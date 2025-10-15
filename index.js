ab4d8ebc3b2aa2a79e177656a0ed5e4eaa4e05c8
durgasahusahu <durgasahusahu1@gmail.com>
2025-10-15 12:21:42 +0530

    created file structer and server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
+const port=process.env.PORT;
+app.listen(port,() => {
+    console.log("server is runnng at ",port);
+})
